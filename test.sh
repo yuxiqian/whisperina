8256566bf008251261637b5046191999480b7b94
yuxiqian <34335406+yuxiqian@users.noreply.github.com>
2025-06-22 15:51:28 +0800

    nit: update test script

--- test.sh
+++ test.sh
@@ -2,7 +2,12 @@
 
 set -ex
 
+# Remove compiled cache first
+rm -rf whisper.cpp/build
+
+# Prepare testing models
 mkdir -p models && cd models
 ../bin/download-ggml-model.sh base
+
 cd ../bin
 DYLD_LIBRARY_PATH=$(pwd) ./whisper-cli -m ../models/ggml-base.bin -f ../samples/jfk.wav -osrt
